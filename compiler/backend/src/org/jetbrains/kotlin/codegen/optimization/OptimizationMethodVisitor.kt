805b72bf8362a793a4204857602d9c0a9abd4ef7
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2022-01-27 19:38:04 +0300

    drop one of method verifier

--- compiler/backend/src/org/jetbrains/kotlin/codegen/optimization/OptimizationMethodVisitor.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/optimization/OptimizationMethodVisitor.kt
@@ -45,7 +45,8 @@ class OptimizationMethodVisitor(
     val normalizationMethodTransformer = CompositeMethodTransformer(
         InplaceArgumentsMethodTransformer(),
         FixStackWithLabelNormalizationMethodTransformer(),
-        MethodVerifier("AFTER mandatory stack transformations", generationState)
+        // TODO is this check really necessary?
+//        MethodVerifier("AFTER mandatory stack transformations", generationState)
     )
 
     val optimizationTransformer = CompositeMethodTransformer(
