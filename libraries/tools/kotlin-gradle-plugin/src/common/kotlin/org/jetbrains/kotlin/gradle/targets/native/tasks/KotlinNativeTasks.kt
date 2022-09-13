cb6d2d4782f96632fccc91a04ecbdcfa09066c1d
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-09-13 23:34:53 +0200

    [Gradle] Extend ResolvedDependencyGraph.dependencyArtifacts to respect...
    external variants. And introduce `moduleArtifacts` to act
    like previous version of `dependencyArtifacts`
    
    Update KotlinNativeTasks accordingly
    
    ^KT-49933

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/KotlinNativeTasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/KotlinNativeTasks.kt
@@ -769,7 +769,7 @@ internal class CacheBuilder(
             .filterIsInstance<ResolvedDependencyResult>()
             .forEach { ensureDependencyPrecached(it, visitedDependencies) }
 
-        val artifactsToAddToCache = dependencyArtifacts(dependency).filter { needCache(it.file.absolutePath) }
+        val artifactsToAddToCache = moduleArtifacts(dependency).filter { needCache(it.file.absolutePath) }
 
         if (artifactsToAddToCache.isEmpty()) return
 
@@ -835,6 +835,7 @@ internal class CacheBuilder(
             getAllDependencies(dependency)
                 .flatMap { dependencyArtifacts(it) }
                 .map { it.file }
+                .distinct()
                 .filterKlibsPassedToCompiler()
                 .forEach {
                     args += "-l"
