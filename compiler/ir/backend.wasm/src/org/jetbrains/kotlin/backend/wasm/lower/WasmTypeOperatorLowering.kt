a8242af06cb21593d76fff668ba3b174eebc7519
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-09-22 16:07:59 +0200

    [WASM] Optimise interop adapters

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmTypeOperatorLowering.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmTypeOperatorLowering.kt
@@ -18,10 +18,7 @@ import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.backend.js.utils.erasedUpperBound
 import org.jetbrains.kotlin.ir.builders.*
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.declarations.IrTypeParameter
-import org.jetbrains.kotlin.ir.declarations.IrVariable
+import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.util.*
@@ -39,6 +36,11 @@ class WasmBaseTypeOperatorTransformer(val context: WasmBackendContext) : IrEleme
     private val builtIns = context.irBuiltIns
 
     private lateinit var builder: DeclarationIrBuilder
+
+
+    override fun visitSimpleFunction(declaration: IrSimpleFunction): IrStatement {
+        return super.visitSimpleFunction(declaration)
+    }
 
     override fun visitTypeOperator(expression: IrTypeOperatorCall): IrExpression {
         super.visitTypeOperator(expression)
@@ -205,6 +207,11 @@ class WasmBaseTypeOperatorTransformer(val context: WasmBackendContext) : IrEleme
         val toClass = toType.eraseToClassOrInterface
 
         if (fromClass.isExternal && toClass.isExternal) {
+            return value
+        }
+
+        if (value.isNullConst() && fromClass.isExternal != toClass.isExternal) {
+            value.type = toType
             return value
         }
 
