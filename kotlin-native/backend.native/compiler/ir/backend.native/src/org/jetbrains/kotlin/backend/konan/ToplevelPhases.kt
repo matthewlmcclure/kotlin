9a6e39dd32e25ffe2f05d9dfbc66934537f53ece
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-10-04 09:27:01 +0300

    [K/N] Remove LocalEscapeAnalysis
    It was disabled for a long time because we have
    a proper one now.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ToplevelPhases.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ToplevelPhases.kt
@@ -400,7 +400,6 @@ internal val bitcodePhase = NamedCompilerPhase(
                 RTTIPhase then
                 generateDebugInfoHeaderPhase then
                 escapeAnalysisPhase then
-                localEscapeAnalysisPhase then
                 codegenPhase then
                 finalizeDebugInfoPhase then
                 cStubsPhase
@@ -519,8 +518,6 @@ internal fun PhaseConfig.konanPhasesConfig(config: KonanConfig) {
         // is workarounded in [FunctionReferenceLowering] by taking erasure of SAM conversion type).
         // Also see https://youtrack.jetbrains.com/issue/KT-50399 for more details.
         disable(checkSamSuperTypesPhase)
-
-        disable(localEscapeAnalysisPhase)
 
         disableIf(singleCompilation, config.producePerFileCache)
         disableUnless(umbrellaCompilation, config.producePerFileCache)
